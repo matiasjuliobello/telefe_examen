e9291d68768a5c29c92947f78f8b8732e805f119
mbello <matias.julio.bello@gmail.com>
2018-09-13 11:32:44 -0300

    Added Database project with SQL script ( "01_CREATE_TABLE.sql" )

---
+++ Telefe_Examen_API/Database/dbo/Scripts/01_CREATE_TABLE.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Search_Record]
+(
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Search] [varchar](50) NOT NULL,
+	[Result] [bit] NOT NULL,
+	[Timestamp] [datetime] NOT NULL,
+	CONSTRAINT [PK_Search_Record] PRIMARY KEY CLUSTERED 
+	(
+		[Id] ASC
+	)
+)
+GO
---
+++ Telefe_Examen_API/Database/dbo/Tables/Search_Record.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Search_Record] (
+    [Id]        INT          IDENTITY (1, 1) NOT NULL,
+    [Search]    VARCHAR (50) NOT NULL,
+    [Result]    BIT          NOT NULL,
+    [Timestamp] DATETIME     NOT NULL,
+    CONSTRAINT [PK_Search_Record] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
